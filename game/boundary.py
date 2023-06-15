130eb06bc4e73a861633ba2308a8bf4325664a7a
S. Dhyanesh <dhyan130709@gmail.com>
2023-06-15 17:17:35 +0530

    (Refractor) Change code structure for game boundary

---
+++ game/boundary.py
@@ -0,0 +1,44 @@
+import pygame
+
+class Boundary:
+    def __init__(self, 
+                 display: pygame.Surface,
+                 color: str = "red",
+                 thickness: int = 6,
+                 stats_sep_y_offset = 65,
+                 ) -> None:
+        self.display = display
+        
+        self.color = color
+        self.thickness = thickness
+        self.stats_sep_y_offset = stats_sep_y_offset
+        self.update_rects()
+        
+    def update_rects(self) -> None:
+        display_width = self.display.get_width()
+        display_height = self.display.get_height()
+        
+        highscore_separator_x = int(display_width / 4)
+        score_separator_x = display_width - highscore_separator_x
+        
+        self.top_line = pygame.Rect(0, 0, display_width, self.thickness + 1.5)
+        self.left_line = pygame.Rect(0, 0, self.thickness, display_height)
+        self.right_line = pygame.Rect(display_width - self.thickness, 0, self.thickness, display_height)
+        self.bottom_line = pygame.Rect(0, display_height - self.thickness, display_width, self.thickness)
+        
+        stats_sep_y = display_height - self.stats_sep_y_offset
+        
+        self.stats_separator = pygame.Rect(0, stats_sep_y, display_width, self.thickness)
+        self.score_separator = pygame.Rect(score_separator_x, stats_sep_y, self.thickness, self.stats_sep_y_offset)
+        self.highscore_separator = pygame.Rect(highscore_separator_x, stats_sep_y, self.thickness, self.stats_sep_y_offset)
+        
+    def render(self) -> None:
+        pygame.draw.rect(self.display, self.color, self.top_line)
+        pygame.draw.rect(self.display, self.color, self.bottom_line)
+        pygame.draw.rect(self.display, self.color, self.left_line)
+        pygame.draw.rect(self.display, self.color, self.right_line)
+        
+        pygame.draw.rect(self.display, self.color, self.stats_separator)
+        pygame.draw.rect(self.display, self.color, self.score_separator)
+        pygame.draw.rect(self.display, self.color, self.highscore_separator)
+        
--- game/game.py
+++ game/game.py
@@ -1,23 +1,12 @@
 import pygame
 import os
 from typing import Tuple, Union
-from dataclasses import dataclass
 
 from sprites.snake import Snake
 from sprites.fruit import Fruit
 from .score import Score
 from .utils import center_of
-
-@dataclass
-class BoundaryLines:
-    line_top: pygame.Rect
-    line_left: pygame.Rect
-    line_right: pygame.Rect
-    line_bottom: pygame.Rect
-    
-    stats_separator: pygame.Rect
-    score_separator: pygame.Rect
-    highscore_separator: pygame.Rect
+from .boundary import Boundary
 
 
 class Game:
@@ -26,17 +15,14 @@ class Game:
 
         self.snake = Snake(display, 15, 15, outline_width=2)
         self.fruit = Fruit(display, 20, 20)
+        self.boundary = Boundary(display)
         
         self.snake.start()
-        
-        self.boundary_color: Union[str, Tuple[int, int, int]] = "red"
-        self.boundary_thickness: int = 6
-        
         self.game_font = pygame.font.Font("assets/fonts/score_font.ttf", 40)
         
         cur_dir = os.path.dirname(__file__)
         self.scoreboard = Score(
-            display = self.display, 
+            display = self.display,
             score_icon_path = f"{cur_dir}/apple.png",
             highscore_icon_path = f"{cur_dir}/trophy.png",
             font = self.game_font
@@ -46,8 +32,8 @@ class Game:
         if self.snake.head.colliding_with(self.fruit.rect):
             self.snake.extend()
             self.fruit.set_random_pos(
-                x_range=(self.boundary_lines.line_left.right, self.boundary_lines.line_right.left),
-                y_range=(self.boundary_lines.line_top.bottom, self.boundary_lines.stats_separator.top)
+                x_range=(self.boundary.left_line.right, self.boundary.right_line.left),
+                y_range=(self.boundary.top_line.bottom, self.boundary.stats_separator.top)
             )
             self.scoreboard.increment_score()
         
@@ -62,39 +48,20 @@ class Game:
             self.snake.left()
         elif event.key == pygame.K_RIGHT:
             self.snake.right()
-        
-    def render_boundary(self) -> BoundaryLines:
-        display_width = self.display.get_width()
-        display_height = self.display.get_height()
-        
-        highscore_separator_x = int(display_width/4)
-        score_separator_x = display_width - highscore_separator_x
-        
-        return BoundaryLines(
-            line_top = pygame.draw.rect(self.display, self.boundary_color, [0, 0, display_width, self.boundary_thickness + 1]),
-            line_left = pygame.draw.rect(self.display, self.boundary_color, [0, 0, self.boundary_thickness, display_height]),
-            line_right = pygame.draw.rect(self.display, self.boundary_color, [display_width - self.boundary_thickness, 0, self.boundary_thickness, display_height]),
-            line_bottom = pygame.draw.rect(self.display, self.boundary_color, [0, display_height - self.boundary_thickness, display_width, self.boundary_thickness]),
-            stats_separator = pygame.draw.rect(self.display, self.boundary_color, [0, display_height - 65, display_width, self.boundary_thickness]),
-            score_separator = pygame.draw.rect(self.display, self.boundary_color, [score_separator_x, display_height - 65, self.boundary_thickness, 65]),
-            highscore_separator = pygame.draw.rect(self.display, self.boundary_color, [highscore_separator_x, display_height - 65, self.boundary_thickness, 65])
-        )
-
     
     def render(self) -> None:
-        self.boundary_lines = self.render_boundary()
         self.point()
         self.snake.render()
         self.fruit.render()
         print(self.snake.body.colliding_with(self.snake.head.rect))
         self.scoreboard.render(
             score_coords = center_of(
-                (self.boundary_lines.score_separator.right, self.boundary_lines.line_right.left),
-                (self.boundary_lines.stats_separator.bottom, self.boundary_lines.line_bottom.top)
+                (self.boundary.score_separator.right, self.boundary.right_line.left),
+                (self.boundary.stats_separator.bottom, self.boundary.bottom_line.top)
             ),
             highscore_coords = center_of(
-                (self.boundary_lines.line_left.right, self.boundary_lines.highscore_separator.left),
-                (self.boundary_lines.stats_separator.bottom, self.boundary_lines.line_bottom.top)
+                (self.boundary.left_line.right, self.boundary.highscore_separator.left),
+                (self.boundary.stats_separator.bottom, self.boundary.bottom_line.top)
             )
         )
-        
+        self.boundary.render()
