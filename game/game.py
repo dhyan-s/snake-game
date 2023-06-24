6cbe997eb6bafa764428d745ce050a0f3ea1048c
S. Dhyanesh <dhyan130709@gmail.com>
2023-06-24 14:57:40 +0530

    (Refractor) Document game.py

--- game/game.py
+++ game/game.py
@@ -12,6 +12,11 @@ from .utils import center_of_rect
 
 class Game:
     def __init__(self, display: pygame.Surface) -> None:
+        """
+        Parameters:
+            display (pygame.Surface): The display surface to render the game on.
+        """
+        
         self.display = display
 
         self.load_fonts()
@@ -19,6 +24,7 @@ class Game:
         self.load_scoreboard()
         
     def load_game_objects(self) -> None:
+        """Initialize the necessary game objects."""
         self.snake = Snake(self.display, 60, 60, outline_width=2)
         self.fruit = Fruit(self.display, 20, 20)
         self.boundary = Boundary(self.display)
@@ -34,6 +40,7 @@ class Game:
         self.change_fruit_pos()
         
     def load_fonts(self) -> None:
+        """Load the fonts used in the game."""
         font_path = "assets/fonts"
         
         game_font_path = f"{font_path}/game_font.ttf"
@@ -44,6 +51,10 @@ class Game:
         self.message_font = pygame.font.Font(message_font_path, 35)
         
     def load_scoreboard(self) -> None:
+        """
+        Initialize the Score class for tracking and rendering the
+        score and highscore.
+        """
         cur_dir = os.path.dirname(__file__)
         self.scoreboard = Score(
             display = self.display,
@@ -53,24 +64,42 @@ class Game:
             )
         
     def point(self) -> None:
+        """
+        Check if the snake collides with the fruit and update the score and snake.
+
+        If the snake's head collides with the fruit, the snake is extended, the fruit position is changed,
+        and the score is incremented.
+        """
         if self.snake.head.colliding_with(self.fruit.rect):
             self.snake.extend()
             self.change_fruit_pos()
             self.scoreboard.increment_score()
             
     def change_fruit_pos(self) -> None:
+        """Change the position of the fruit to a random coordinate within the game boundaries."""
         self.fruit.set_random_pos(
             x_range=(self.boundary.left_line.right, self.boundary.right_line.left),
             y_range=(self.boundary.top_line.bottom, self.boundary.stats_separator.top)
         )
         
     def restart_game(self) -> None:
+        """Restart the snake game."""
         self.scoreboard.reset_score()
         self.snake.reset()
         self.change_fruit_pos()
         self.snake.start()
         
     def handle_event(self, event: pygame.event.Event):
+        """
+        Handle the keyboard events for controlling the snake and restarting the game.
+
+        - This method handles the pygame.KEYDOWN event and determines the action based on the pressed key.
+        - Arrow keys control the snake's movement, the space and enter keys trigger game restart.
+
+        Parameters:
+            event (pygame.event.Event): The pygame event to handle.
+        """
+        
         if event.type != pygame.KEYDOWN:
             return
         
@@ -96,6 +125,11 @@ class Game:
             self.gameover_handler.reset()
             
     def render_title(self) -> None:
+        """
+        Render the title text on the display surface.
+
+        The title text is rendered in between the score and high score.
+        """
         title_text = self.game_font.render("SNAKE GAME BY DHYANESH !!" , True , "white")
         title_rect = title_text.get_rect(center=center_of_rect(
             (self.boundary.highscore_separator.right, self.boundary.score_separator.left),
@@ -104,6 +138,10 @@ class Game:
         self.display.blit(title_text, title_rect)
     
     def render(self) -> None:
+        """
+        Render the game objects such as the snake, fruit, score, boundaries 
+        and title on the display surface.
+        """
         self.gameover_handler.handle_game_over()
         self.point()
         self.snake.render()
