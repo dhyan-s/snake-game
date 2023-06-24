f003775b0c387299892a0c0a8e5d0f06f795a183
S. Dhyanesh <dhyan130709@gmail.com>
2023-06-24 17:45:55 +0530

    (Refractor) Document game_over.py

--- game/gameover.py
+++ game/gameover.py
@@ -6,6 +6,10 @@ from .boundary import Boundary
 from .utils import center_of, center_of_rect
 
 class GameOver:
+    """
+    Manages the game over state and rendering of game over text on the display surface.
+    """
+    
     def __init__(self, 
                  display: pygame.Surface, 
                  snake: Snake, 
@@ -15,6 +19,16 @@ class GameOver:
                  gameover_callback: Callable = None,
                  restart_callback: Callable = None
                  ):
+        """
+        Parameters:
+            display (pygame.Surface): The display surface to render the game over text on.
+            snake (Snake): The Snake object.
+            boundary (Boundary): The Boundary object.
+            gameover_font (pygame.font.Font): The font for the game over text.
+            message_font (pygame.font.Font): The font for the additional messages.
+            gameover_callback (Callable): The callback function to be triggered on game over.
+            restart_callback (Callable): The callback function to be triggered on restart.
+        """
         self.display = display
         self.__snake = snake
         self.__boundary = boundary
@@ -29,6 +43,7 @@ class GameOver:
         self.reset()
         
     def reset(self) -> None:
+        """Reset the game over state and trigger restart callback if available."""
         if not self.game_over: 
             return
         self.game_over = False
@@ -37,6 +52,12 @@ class GameOver:
             self.restart_callback()
     
     def check_game_over(self) -> Tuple[bool, str]:
+        """
+        Check for game over conditions, such as collision with boundaries or the snake's own body.
+
+        Returns:
+            Tuple[bool, str]: A tuple containing a boolean indicating game over status and a string representing the reason for game over.
+        """
         if self.__snake.body.colliding_with(self.__snake.head.rect):
             return (True, "SNAKE BUMPED INTO ITSELF")
         elif any(self.__snake.colliding_with(rect) for rect in [self.__boundary.top_line, 
@@ -48,6 +69,7 @@ class GameOver:
         return (False, "")
     
     def handle_game_over(self) -> None:
+        """Trigger game over callback and render game over text if the game over conditions are met."""
         if not self.game_over:
             self.game_over, self.reason = self.check_game_over()
             if self.game_over and self.gameover_callback is not None:
@@ -55,6 +77,7 @@ class GameOver:
         self.render_gameover_text()
     
     def render_gameover_text(self) -> None:
+        """Render the game over text on the display surface if the game_over flag is set to true."""
         if not self.game_over: return
         game_over_text = self.gameover_font.render("GAMEOVER!" , True , "white")
         reason_text = self.message_font.render(f"REASON: {self.reason}" , False , "white") 
